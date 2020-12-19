a8dee095b974891f284ed3da2ecf5c277d14526f
Ian Edwards <ian.philip.edwards@gmail.com>
2020-12-19 21:47:57 +0000

    working proof of concept needs content and refactor

--- background/background.js
+++ background/background.js
@@ -1,4 +1,12 @@
-chrome.browserAction.onClicked.addListener(sendfunc);
-function sendfunc(tab){
-    alert("HELLO WORLD")
-}
+chrome.browserAction.onClicked.addListener(function(activeTab){
+    const search = window.prompt("Enter search text: ");
+
+    const searchPlus = search.replace(" ", "+");
+    chrome.tabs.create({ url: `https://www.amazon.co.uk/s?k=${searchPlus}` });
+
+    const searchMinus = search.replace(" ", "-");
+    chrome.tabs.create({ url: `https://www.argos.co.uk/search/${searchMinus}/` });
+
+    const searchSpace = search.replace(" ", "%20");
+    chrome.tabs.create({ url: `https://www.coolshop.co.uk/s/?q=${searchSpace}` });
+  });
