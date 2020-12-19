5d18b1eeb466bfeb267fdbb43f304d4978ec1416
Ian Edwards <ian.philip.edwards@gmail.com>
2020-12-19 21:32:20 +0000

    syntax is working but some issue in opening multiple tabs

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,20 @@
 var search = window.prompt("Enter search text: ");
-var searchPlus = search.replace(" ", "+");
-var searchMinus = search.replace(" ", "-");
 var searchSpace = search.replace(" ", "%20");
 
-alert(searchSpace)
+const timeoutinterval = 1000;
+var timeoutstep = 0;
 
-window.open(`https://www.amazon.co.uk/s?k=${searchPlus}`);
+var searchPlus = search.replace(" ", "+");
+setTimeout(function(){ window.open(`https://www.amazon.co.uk/s?k=${searchPlus}`); }, timeoutstep++ * timeoutinterval);
+setTimeout(function(){ window.open(`https://www.base.com/fsearch.htm?search=${searchPlus}`); }, timeoutstep++ * timeoutinterval);
 
-window.open(`https://www.argos.co.uk/search/${searchMinus}/`);
+var searchMinus = search.replace(" ", "-");
+setTimeout(function(){ window.open(`https://www.argos.co.uk/search/${searchMinus}/`); }, timeoutstep++ * timeoutinterval);
+//window.open(`https://www.amazon.co.uk/s?k=${searchPlus}`);
+//window.open(`https://www.base.com/fsearch.htm?search=${searchPlus}`);
 
-window.open(`https://uk.webuy.com/search?stext=${searchSpace}`);
+//window.open(`https://www.argos.co.uk/search/${searchMinus}/`);
+
+var searchSpace = search.replace(" ", "%20");
+setTimeout(function(){ window.open(`https://www.coolshop.co.uk/s/?q=${searchSpace}`); }, timeoutstep++ * timeoutinterval);
+//window.open(`https://www.coolshop.co.uk/s/?q=${searchSpace}`);
