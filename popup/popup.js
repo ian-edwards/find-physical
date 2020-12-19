de0fbf3021af70bacfee3dd3f6642e0a5c0200b8
Ian Edwards <ian.philip.edwards@gmail.com>
2020-12-19 20:54:31 +0000

    plus and minus search proof of concept working search space not working

--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,12 @@
-var name = window.prompt("Enter your name: ");
-window.open("https://www.w3schools.com");
+var search = window.prompt("Enter search text: ");
+var searchPlus = search.replace(" ", "+");
+var searchMinus = search.replace(" ", "-");
+var searchSpace = search.replace(" ", "%20");
+
+alert(searchSpace)
+
+window.open(`https://www.amazon.co.uk/s?k=${searchPlus}`);
+
+window.open(`https://www.argos.co.uk/search/${searchMinus}/`);
+
+window.open(`https://uk.webuy.com/search?stext=${searchSpace}`);
